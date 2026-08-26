aeecb55e6527ff8609828b7a5c80f984ad4e8a52
jbt <jbt@114-04>
2026-08-26 10:45:26 +0300

    added skeleton functions

---
+++ POC-Data-compression/POC_Data_compression.py
@@ -0,0 +1,44 @@
+
+
+
+
+def main():
+    pass
+
+
+def get_string_data(string_data): # temporary
+    pass
+
+# - get_data(file_path)
+#
+def get_data(file_path):
+    pass
+
+def get_frequency(file_data):
+    pass
+
+def build_minheap(data_frequency):
+    pass
+
+def get_huffman_btree():
+    pass
+
+def build_frequency_dict():
+    pass
+
+def encrypt_huffmans():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
