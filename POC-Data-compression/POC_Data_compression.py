9c1e2cca543ebded9b4efb6a8b4ee57e7e762e41
jbt <jbt@114-04>
2026-08-26 14:52:27 +0300

    encryption working, code needs to be prettier

--- POC-Data-compression/POC_Data_compression.py
+++ POC-Data-compression/POC_Data_compression.py
@@ -1,17 +1,30 @@
+from base64 import encode
 import heapq
 
-
+class Node:
+    def __init__(self, frequency, index, left=None, right=None):
+        self.frequency = frequency
+        self.index = index
+        self.left = left
+        self.right = right
 
 def main():
     file_data = ""
-    frequency_dict = {}
+    frequency_list = []
     frequency_minheap = []
+    keys_dict = {}
+    encoded_file = []
     
-    
+    file_data = get_string_data()
+    frequency_list = get_frequency(file_data)
+    keys_dict = get_huffman_encoding(frequency_list)
+    encoded_file = encode(keys_dict, file_data)
+    for letter in encoded_file:
+        print(letter, end=" ")
 
 
-def get_string_data(string_data): # temporary
-    pass
+def get_string_data(): # temporary
+    return input("Enter the string data:\n")
 
 # - get_data(file_path)
 # This function gets a file path, it then proceeds to open it and read the data and returns it
@@ -27,44 +40,71 @@ def get_data(file_path):
 # Input : file_data - str
 # Output : frequency_dict - dictionary
 def get_frequency(file_data):
-    pass
+    frequency_dict = {}
+    sorted_frequency_list = []
+    
+    for letter in file_data:
+        if letter in frequency_dict:
+            frequency_dict[letter] += 1
+        else:
+            frequency_dict[letter] = 1
+    for letter, frequency in sorted(frequency_dict.items(), key=lambda item: item[1]):
+        sorted_frequency_list.append([letter, frequency])
+    return sorted_frequency_list
 
-# - build_minheap(frequency_dict)
-# This function get a dict with all of the letters that have shown up in the file data as a key and the frequency they showed up
-# as the keys value.
-# The purpose of this function is to build a min-heap where the letters with the smallest frequency have the biggest priority
-# and the letters with the most frequency have the lowest priority (saved as tuples, first value is the letter and second value is the frequency).
-# Input : frequency_dict - dictionary
-# Output: frequency_minheap - heap
-def build_minheap(frequency_dict):
-    pass
 
-# - get_huffman_btree(frequency_minheap)
-# This function gets a heap of all of the letters and their frequencies as tuples by min-value.
-# The function creates a binary huffman tree and returns it.
-# Input : frequency_minheap - heap
-# Output : huffman_btree - binary tree
-def get_huffman_btree(frequency_minheap):
-    pass
 
-# - build_code_dict(huffman_btree)
-# This function gets a huffman binary tree of all of the letters and their frequencies sorted.
-# The function creates the codes for all of the appearing letters inside the file.
-# It does this by going either left or right in the binary tree, for each left turn it adds a '0'
-# to the code and for each right turn it adds a '1'. the code is decided when after all the turns it reaches a letter.
-# Input: huffman_btree - binary tree
-# Output: code_dict - dictionary
-def build_code_dict(huffman_btree):
-    pass
+def encode_letters(root, carried_letter, encoded_file):
+    if root is None:
+        return
 
-# - encrypt(code_dict):
-# This function gets a dictionary of all of the letters appearing in the file data as keys and their code as values,
-# aswell as the file data itself.
-# The function goes over the file data and for each letter insted inserts the code for that letter.
-# Input : code_dict - dictionary, file_data - str
-# Output: encryped_file - bytestr
-def encrypt(code_dict, file_data):
-    pass
+    if root.left is None and root.right is None:
+        if carried_letter == "":
+            carried_letter = "0"
+        encoded_file.append(carried_letter)
+        return
+
+    encode_letters(root.left, carried_letter + '0', encoded_file)
+    encode_letters(root.right, carried_letter + '1', encoded_file)
+
+
+
+def get_huffman_encoding(frequency_list):
+    keys_dict = {}
+    encoded_file = []
+    min_heap = []
+
+    for i in range(len(frequency_list)):
+        temp_node = Node(frequency_list[i][1], i)
+        heapq.heappush(min_heap, (temp_node.frequency, temp_node.index, temp_node))
+
+    if len(frequency_list) == 1 and frequency_list[0][1] == 1:
+        return ["0"]
+
+    while len(min_heap) >= 2:
+
+        freq_left, indx_left, left = heapq.heappop(min_heap) # Left node
+        freq_right, indx_right, right = heapq.heappop(min_heap) # Right node
+
+        newNode = Node(left.frequency + right.frequency, min(left.index, right.index), left, right) # Creates a new node with both of the frequencies
+        heapq.heappush(min_heap, (newNode.frequency, newNode.index, newNode)) # Pushes the new node inside the heap
+
+    root = min_heap[0][2] # The first index is 0, and the second index (2) is the node itself (the root)
+    encode_letters(root, "", encoded_file)
+    for i in range(len(frequency_list)):
+        keys_dict[frequency_list[i][0]] = encoded_file[i]
+    return keys_dict
+
+
+def encode(keys_dict, file_data):
+    encoded_file = []
+
+    for letter in file_data:
+        encoded_file.append(keys_dict[letter])
+    return encoded_file
+
+
+
 
 
 
